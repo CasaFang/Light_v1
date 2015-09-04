b8079113c50ee6c04e6e0ba02c2ad27c9f13ffd4
saberge <saberge@163.com>
2015-09-04 21:40:13 +0800

    测试svn git

--- Light/AppDelegate.m
+++ Light/AppDelegate.m
@@ -56,7 +56,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     //Mob短信验证SDK初始化
-    // test svn git 2
+    // test svn git 22
     [SMS_SDK registerApp:LightKey withSecret:LightSecret];
     
 //    NSString *_deviceTokenCache = [[NSUserDefaults standardUserDefaults]objectForKey:kDeviceToken];
